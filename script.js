ba043244235409a5f16a5d2363d776def2acb96f
khs <ksalumiev@gmail.com>
2022-10-05 14:55:36 +0300

    Complete click events

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const display = document.querySelector('p');
+const empty_array = document.querySelector('.clear');
+const backspace = document.querySelector('.backspace');
+const decimal = document.querySelector('.decimal');
+
 let result = 0;
 
 const add = function (x, y) {
@@ -41,11 +48,11 @@ const operate = function (operator, x, y) {
             solution = divide(x, y);
             break;
     }
-    return solution;
+    return solution.toString();
 }
 
 const evaluateCalculation = function () {
-    display.textContent = operate(user_input[1],user_input[0],user_input[2]);
+    display.textContent = operate(user_input[1],user_input[0],user_input[2]).slice(0,20);
     if (user_input[3] === "="){
         user_input.splice(0,user_input.length)
     } else {
@@ -58,11 +65,12 @@ const evaluateCalculation = function () {
   
 }
 
-const numbers = document.querySelectorAll('.number');
-const operators = document.querySelectorAll('.operator');
-const display = document.querySelector('p');
-const empty_array = document.querySelector('.clear');
-const backspace = document.querySelector('.backspace');
+// const setDisplayContent = function (text) {
+//     if ( text.length > 19 ) {
+//         text = text.slice(0 , 20);
+//     }
+//     display.textContent = text;
+// }
 
 let display_content = display.textContent;
 let first_number = 0;
@@ -81,8 +89,10 @@ numbers.forEach( (number) => {
             display.textContent = "";
             isLastOperator = false;
         }
-        display.textContent += number.textContent;
 
+        if ( display.textContent.length < 20 ) {
+        display.textContent += number.textContent;
+        }
     })
 })
 
@@ -115,4 +125,11 @@ empty_array.addEventListener('click', () => {
 backspace.addEventListener('click', () => {
     display.textContent = display.textContent.slice(0, display.textContent.length - 1);
 })
+
+decimal.addEventListener('click', () => {
+    if (!display.textContent.includes('.') && display.textContent.length < 19) {
+        display.textContent += '.';        
+    }
+})
+
 
